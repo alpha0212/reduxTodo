b8b4ca2c4780ba0baa5bcccafed83f068e84aa9c
alpha0212 <qkrcjfgml321@naver.com>
2023-01-02 21:59:15 +0900

    Redux: action

---
+++ src/actions/index.ts
@@ -0,0 +1,22 @@
+export const addTodo = (data: string) => {
+  return {
+    type: "ADD_TODO",
+    payload: {
+      id: new Date().getTime().toString(),
+      data: data,
+    },
+  };
+};
+
+export const deleteTodo = (id: any) => {
+  return {
+    type: "DELETE_TODO",
+    id,
+  };
+};
+
+export const removeTodo = () => {
+  return {
+    type: "REMOVE_TODO",
+  };
+};
